3f5ad0a0612b35ba420410552a933d3afff1b7f6
Nolandexco <stemapal321@gmail.com>
2025-06-20 20:16:00 +0700

    Update team-section.js

--- src/sections/team-section.js
+++ src/sections/team-section.js
@@ -5,10 +5,8 @@ import SectionHeader from "components/section-header";
 import TeamCard from "components/team-card";
 import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
 
-// 1. Impor data
 import data from "config/data.json";
 
-// Impor aset gambar
 import Member1 from "assets/team/member-1.png";
 import Member2 from "assets/team/member-2.png";
 import Member3 from "assets/team/member-3.png";
@@ -16,10 +14,8 @@ import Member4 from "assets/team/member-4.png";
 import Member5 from "assets/team/member-5.png";
 import Member6 from "assets/team/member-6.png";
 
-// 2. Ambil data yang relevan
 const { team } = data;
 
-// 4. Peta untuk gambar dan ikon
 const imageMap = {
 	"assets/team/member-1.png": Member1,
 	"assets/team/member-2.png": Member2,
@@ -28,6 +24,7 @@ const imageMap = {
 	"assets/team/member-5.png": Member5,
 	"assets/team/member-6.png": Member6,
 };
+
 const iconMap = {
 	facebook: <FaFacebookF />,
 	twitter: <FaTwitter />,
@@ -38,11 +35,9 @@ export default function TeamSection() {
 	return (
 		<section>
 			<Container>
-				{/* 3. Gunakan data dari JSON */}
 				<SectionHeader slogan={team.slogan} title={team.title} />
 				<Grid sx={styles.grid}>
 					{team.members.map((item) => {
-						// Ubah profil sosial untuk menyertakan komponen ikon
 						const socialProfilesWithIcons = item.socialProfile.map(
 							(profile) => ({
 								...profile,
@@ -52,11 +47,11 @@ export default function TeamSection() {
 						return (
 							<TeamCard
 								key={item.id}
-								src={imageMap[item.imgSrc]} // Gunakan peta gambar
+								src={imageMap[item.imgSrc]}
 								altText={item.altText}
 								title={item.title}
 								designation={item.designation}
-								social={socialProfilesWithIcons} // Gunakan profil sosial yang sudah diubah
+								social={socialProfilesWithIcons}
 							/>
 						);
 					})}
@@ -66,7 +61,16 @@ export default function TeamSection() {
 	);
 }
 
-// Styles tidak perlu diubah
 const styles = {
-    // ... (kode styles sama seperti sebelumnya)
+	grid: {
+		mt: [0, null, -6, null, -4],
+		gridGap: ["35px 0px", null, 0, null, null, "30px 35px"],
+		gridTemplateColumns: [
+			"repeat(2,1fr)",
+			null,
+			"repeat(2,1fr)",
+			null,
+			"repeat(3,1fr)",
+		],
+	},
 };
