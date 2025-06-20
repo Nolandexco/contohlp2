f015e5539da832c84e127ae6aae5ecb59b605023
Nolandexco <stemapal321@gmail.com>
2025-06-20 20:02:06 +0700

    Update team-section.js

--- src/sections/team-section.js
+++ src/sections/team-section.js
@@ -5,6 +5,10 @@ import SectionHeader from "components/section-header";
 import TeamCard from "components/team-card";
 import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
 
+// 1. Impor data
+import data from "config/data.json";
+
+// Impor aset gambar
 import Member1 from "assets/team/member-1.png";
 import Member2 from "assets/team/member-2.png";
 import Member3 from "assets/team/member-3.png";
@@ -12,206 +16,57 @@ import Member4 from "assets/team/member-4.png";
 import Member5 from "assets/team/member-5.png";
 import Member6 from "assets/team/member-6.png";
 
-const data = [
-	{
-		id: 1,
-		imgSrc: Member1,
-		altText: "Saimon Harmer",
-		title: "Saimon Harmer",
-		designation: "CEO and Founder",
-		socialProfile: [
-			{
-				id: 1,
-				name: "facebook",
-				path: "#",
-				icon: <FaFacebookF />,
-			},
-			{
-				id: 2,
-				name: "twitter",
-				path: "#",
-				icon: <FaTwitter />,
-			},
-			{
-				id: 3,
-				name: "instagram",
-				path: "#",
-				icon: <FaInstagram />,
-			},
-		],
-	},
-	{
-		id: 2,
-		imgSrc: Member2,
-		altText: "Aaron Nunez",
-		title: "Aaron Nunez",
-		designation: "Founder",
-		socialProfile: [
-			{
-				id: 1,
-				name: "facebook",
-				path: "#",
-				icon: <FaFacebookF />,
-			},
-			{
-				id: 2,
-				name: "twitter",
-				path: "#",
-				icon: <FaTwitter />,
-			},
-			{
-				id: 3,
-				name: "instagram",
-				path: "#",
-				icon: <FaInstagram />,
-			},
-		],
-	},
-	{
-		id: 3,
-		imgSrc: Member3,
-		altText: "Aaron Nunez",
-		title: "Aaron Nunez",
-		designation: "Web Designer",
-		socialProfile: [
-			{
-				id: 1,
-				name: "facebook",
-				path: "#",
-				icon: <FaFacebookF />,
-			},
-			{
-				id: 2,
-				name: "twitter",
-				path: "#",
-				icon: <FaTwitter />,
-			},
-			{
-				id: 3,
-				name: "instagram",
-				path: "#",
-				icon: <FaInstagram />,
-			},
-		],
-	},
-	{
-		id: 4,
-		imgSrc: Member4,
-		altText: "Lina Jutila",
-		title: "Lina Jutila",
-		designation: "Web Developer",
-		socialProfile: [
-			{
-				id: 1,
-				name: "facebook",
-				path: "#",
-				icon: <FaFacebookF />,
-			},
-			{
-				id: 2,
-				name: "twitter",
-				path: "#",
-				icon: <FaTwitter />,
-			},
-			{
-				id: 3,
-				name: "instagram",
-				path: "#",
-				icon: <FaInstagram />,
-			},
-		],
-	},
-	{
-		id: 5,
-		imgSrc: Member5,
-		altText: "Saimon Harmer",
-		title: "Saimon Harmer",
-		designation: "CEO and Founder",
-		socialProfile: [
-			{
-				id: 1,
-				name: "facebook",
-				path: "#",
-				icon: <FaFacebookF />,
-			},
-			{
-				id: 2,
-				name: "twitter",
-				path: "#",
-				icon: <FaTwitter />,
-			},
-			{
-				id: 3,
-				name: "instagram",
-				path: "#",
-				icon: <FaInstagram />,
-			},
-		],
-	},
-	{
-		id: 6,
-		imgSrc: Member6,
-		altText: "Aaron Nunez",
-		title: "Aaron Nunez",
-		designation: "Web Designer",
-		socialProfile: [
-			{
-				id: 1,
-				name: "facebook",
-				path: "#",
-				icon: <FaFacebookF />,
-			},
-			{
-				id: 2,
-				name: "twitter",
-				path: "#",
-				icon: <FaTwitter />,
-			},
-			{
-				id: 3,
-				name: "instagram",
-				path: "#",
-				icon: <FaInstagram />,
-			},
-		],
-	},
-];
+// 2. Ambil data yang relevan
+const { team } = data;
+
+// 4. Peta untuk gambar dan ikon
+const imageMap = {
+	"assets/team/member-1.png": Member1,
+	"assets/team/member-2.png": Member2,
+	"assets/team/member-3.png": Member3,
+	"assets/team/member-4.png": Member4,
+	"assets/team/member-5.png": Member5,
+	"assets/team/member-6.png": Member6,
+};
+const iconMap = {
+	facebook: <FaFacebookF />,
+	twitter: <FaTwitter />,
+	instagram: <FaInstagram />,
+};
 
 export default function TeamSection() {
 	return (
 		<section>
 			<Container>
-				<SectionHeader
-					slogan="our team"
-					title="The most qualified and talented individuals"
-				/>
+				{/* 3. Gunakan data dari JSON */}
+				<SectionHeader slogan={team.slogan} title={team.title} />
 				<Grid sx={styles.grid}>
-					{data.map((item) => (
-						<TeamCard
-							key={item.id}
-							src={item.imgSrc}
-							altText={item.altText}
-							title={item.title}
-							designation={item.designation}
-							social={item.socialProfile}
-						/>
-					))}
+					{team.members.map((item) => {
+						// Ubah profil sosial untuk menyertakan komponen ikon
+						const socialProfilesWithIcons = item.socialProfile.map(
+							(profile) => ({
+								...profile,
+								icon: iconMap[profile.name],
+							})
+						);
+						return (
+							<TeamCard
+								key={item.id}
+								src={imageMap[item.imgSrc]} // Gunakan peta gambar
+								altText={item.altText}
+								title={item.title}
+								designation={item.designation}
+								social={socialProfilesWithIcons} // Gunakan profil sosial yang sudah diubah
+							/>
+						);
+					})}
 				</Grid>
 			</Container>
 		</section>
 	);
 }
 
+// Styles tidak perlu diubah
 const styles = {
-	grid: {
-		mt: [0, null, -6, null, -4],
-		gridGap: ["35px 0px", null, 0, null, null, "30px 35px"],
-		gridTemplateColumns: [
-			"repeat(2,1fr)",
-			null,
-			"repeat(2,1fr)",
-			null,
-			"repeat(3,1fr)",
-		],
-	},
+    // ... (kode styles sama seperti sebelumnya)
 };
