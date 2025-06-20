a6334c8d95689ce84e1690fc8a9ae63ac0b23174
Nolandexco <stemapal321@gmail.com>
2025-06-20 23:43:55 +0700

    Update team-section.js

--- src/sections/team-section.js
+++ src/sections/team-section.js
@@ -1,3 +1,4 @@
+// components/team-section.js
 /** @jsx jsx */
 import { jsx } from "theme-ui";
 import { Container, Grid } from "theme-ui";
@@ -16,46 +17,58 @@ import Member6 from "assets/team/member-6.png";
 const { team } = data;
 
 const imageMap = {
-	"assets/team/member-1.png": Member1,
-	"assets/team/member-2.png": Member2,
-	"assets/team/member-3.png": Member3,
-	"assets/team/member-4.png": Member4,
-	"assets/team/member-5.png": Member5,
-	"assets/team/member-6.png": Member6,
+  "assets/team/member-1.png": Member1,
+  "assets/team/member-2.png": Member2,
+  "assets/team/member-3.png": Member3,
+  "assets/team/member-4.png": Member4,
+  "assets/team/member-5.png": Member5,
+  "assets/team/member-6.png": Member6,
 };
 
 export default function TeamSection() {
-	return (
-		<section>
-			<Container>
-				<SectionHeader slogan={team.slogan} title={team.title} />
-				<Grid sx={styles.grid}>
-					{team.members.map((item) => (
-						<TeamCard
-							key={item.id}
-							src={imageMap[item.imgSrc]}
-							altText={item.altText}
-							title={item.title}
-							designation={item.designation}
-							// Menghapus prop social
-						/>
-					))}
-				</Grid>
-			</Container>
-		</section>
-	);
+  return (
+    <section>
+      <Container>
+        <SectionHeader slogan={team.slogan} title={team.title} />
+        <Grid sx={styles.grid}>
+          {team.members.map((item) => (
+            <TeamCard
+              key={item.id}
+              src={imageMap[item.imgSrc]}
+              altText={item.altText}
+              title={item.title}
+              designation={item.designation}
+            />
+          ))}
+        </Grid>
+      </Container>
+    </section>
+  );
 }
 
 const styles = {
-	grid: {
-		mt: [0, null, -6, null, -4],
-		gridGap: ["35px 0px", null, 0, null, null, "30px 35px"],
-		gridTemplateColumns: [
-			"repeat(2,1fr)",
-			null,
-			"repeat(2,1fr)",
-			null,
-			"repeat(3,1fr)",
-		],
-	},
+  grid: {
+    mt: [0, null, -6, null, -4],
+    gridGap: ["35px 0px", null, 0, null, null, "30px 35px"],
+    gridTemplateColumns: [
+      "repeat(2,1fr)",
+      null,
+      "repeat(2,1fr)",
+      null,
+      "repeat(3,1fr)",
+    ],
+  },
 };
+
+// components/team-card.js
+import { jsx } from "theme-ui";
+
+export default function TeamCard({ src, altText, title, designation }) {
+  return (
+    <div sx={{ textAlign: "center" }}>
+      <img src={src} alt={altText} sx={{ width: "100%", maxWidth: "200px" }} />
+      <h3>{title}</h3>
+      <p>{designation}</p>
+    </div>
+  );
+}
