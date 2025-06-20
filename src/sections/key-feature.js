a937a8bc9b785e63ea3308866e578d4754247764
Nolandexco <stemapal321@gmail.com>
2025-06-20 20:17:24 +0700

    Update key-feature.js

--- src/sections/key-feature.js
+++ src/sections/key-feature.js
@@ -4,19 +4,15 @@ import { Container, Grid } from "theme-ui";
 import SectionHeader from "../components/section-header";
 import FeatureCardColumn from "components/feature-card-column.js";
 
-// 1. Impor data
 import data from "config/data.json";
 
-// Impor semua ikon SVG
 import Performance from "assets/key-feature/performance.svg";
 import Partnership from "assets/key-feature/partnership.svg";
 import Subscription from "assets/key-feature/subscription.svg";
 import Support from "assets/key-feature/support.svg";
 
-// 2. Ambil data yang relevan
 const { keyFeature } = data;
 
-// 4. Buat peta ikon
 const iconMap = {
 	"assets/key-feature/performance.svg": Performance,
 	"assets/key-feature/partnership.svg": Partnership,
@@ -28,7 +24,6 @@ export default function KeyFeature() {
 	return (
 		<section sx={{ variant: "section.keyFeature" }} id="feature">
 			<Container>
-				{/* 3. Gunakan data dari JSON */}
 				<SectionHeader
 					slogan={keyFeature.slogan}
 					title={keyFeature.title}
@@ -37,7 +32,7 @@ export default function KeyFeature() {
 					{keyFeature.items.map((item) => (
 						<FeatureCardColumn
 							key={item.id}
-							src={iconMap[item.imgSrc]} // Gunakan peta ikon
+							src={iconMap[item.imgSrc]}
 							alt={item.altText}
 							title={item.title}
 							text={item.text}
@@ -49,7 +44,25 @@ export default function KeyFeature() {
 	);
 }
 
-// Styles tidak perlu diubah
 const styles = {
-    // ... (kode styles sama seperti sebelumnya)
+	grid: {
+		width: ["100%", "80%", "100%"],
+		mx: "auto",
+		gridGap: [
+			"35px 0",
+			null,
+			"40px 40px",
+			"50px 60px",
+			"30px",
+			"50px 40px",
+			"55px 90px",
+		],
+		gridTemplateColumns: [
+			"repeat(1,1fr)",
+			null,
+			"repeat(2,1fr)",
+			null,
+			"repeat(4,1fr)",
+		],
+	},
 };
