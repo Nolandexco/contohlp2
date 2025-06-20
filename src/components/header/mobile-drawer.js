134a224e9970883ed83295a75bb5a108784b64a0
Nolandexco <stemapal321@gmail.com>
2025-06-20 22:50:06 +0700

    Update mobile-drawer.js

--- src/components/header/mobile-drawer.js
+++ src/components/header/mobile-drawer.js
@@ -1,188 +1,102 @@
 import React, { useState } from "react";
 import { Box } from "theme-ui";
-import { Scrollbars } from "react-custom-scrollbars";
-import Drawer from "components/drawer";
 import { IoMdClose, IoMdMenu } from "react-icons/io";
 import { Link } from "react-scroll";
-import {
-	FaFacebookF,
-	FaTwitter,
-	FaGithubAlt,
-	FaDribbble,
-} from "react-icons/fa";
 import menuItems from "./header.data";
 
-const social = [
-	{
-		path: "/",
-		icon: <FaFacebookF />,
-	},
-	{
-		path: "/",
-		icon: <FaTwitter />,
-	},
-	{
-		path: "/",
-		icon: <FaGithubAlt />,
-	},
-	{
-		path: "/",
-		icon: <FaDribbble />,
-	},
-];
-
 export default function MobileDrawer() {
-	const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-	return (
-		<Drawer
-			width="320px"
-			drawerHandler={
-				<Box sx={styles.handler}>
-					<IoMdMenu size="26px" />
-				</Box>
-			}
-			open={isDrawerOpen}
-			toggleHandler={() => setIsDrawerOpen((prevState) => !prevState)}
-			closeButton={<IoMdClose size="24px" color="#000" />}
-			drawerStyle={styles.drawer}
-			closeBtnStyle={styles.close}
-		>
-			<Scrollbars autoHide>
-				<Box sx={styles.content}>
-					<Box sx={styles.menu}>
-						{menuItems.map((menuItem, i) => (
-							<Link
-								activeClass="active"
-								to={menuItem.path}
-								spy={true}
-								smooth={true}
-								offset={-70}
-								duration={500}
-								key={i}
-							>
-								{menuItem.label}
-							</Link>
-						))}
-					</Box>
-					<Box sx={styles.menuFooter}>
-						<Box sx={styles.social}>
-							{social.map((socialItem, i) => (
-								<Box as="span" key={i} sx={styles.social.icon}>
-									<Link to={socialItem.path}>{socialItem.icon}</Link>
-								</Box>
-							))}
-						</Box>
-					</Box>
-				</Box>
-			</Scrollbars>
-		</Drawer>
-	);
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Box sx={styles.wrapper}>
+      {/* Menu button */}
+      <Box sx={styles.handler} onClick={() => setIsOpen(true)}>
+        <IoMdMenu size="26px" />
+      </Box>
+
+      {/* Mobile menu overlay */}
+      {isOpen && (
+        <Box sx={styles.menuOverlay}>
+          {/* Close button */}
+          <Box sx={styles.closeButton} onClick={() => setIsOpen(false)}>
+            <IoMdClose size="24px" />
+          </Box>
+
+          {/* Menu items */}
+          <Box sx={styles.menuItems}>
+            {menuItems.map((menuItem, i) => (
+              <Link
+                key={i}
+                activeClass="active"
+                to={menuItem.path}
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                onClick={() => setIsOpen(false)}
+                sx={styles.menuLink}
+              >
+                {menuItem.label}
+              </Link>
+            ))}
+          </Box>
+        </Box>
+      )}
+    </Box>
+  );
 }
 
 const styles = {
-	handler: {
-		display: "flex",
-		alignItems: "center",
-		justifyContent: "center",
-		flexShrink: "0",
-		width: "26px",
-
-		"@media screen and (min-width: 1024px)": {
-			display: "none",
-		},
-	},
-
-	drawer: {
-		width: "100%",
-		height: "100%",
-		backgroundColor: "dark",
-	},
-
-	close: {
-		display: "flex",
-		alignItems: "center",
-		justifyContent: "center",
-		position: "absolute",
-		top: "25px",
-		right: "30px",
-		zIndex: "1",
-		cursor: "pointer",
-	},
-
-	content: {
-		width: "100%",
-		height: "100%",
-		display: "flex",
-		flexDirection: "column",
-		pt: "100px",
-		pb: "40px",
-		px: "30px",
-	},
-
-	menu: {
-		width: "100%",
-		display: "flex",
-		flexDirection: "column",
-		a: {
-			fontSize: "16px",
-			fontWeight: "500",
-			color: "text_white",
-			py: "15px",
-			cursor: "pointer",
-			borderBottom: "1px solid #e8e5e5",
-			transition: "all 0.25s",
-			"&:hover": {
-				color: "secondary",
-			},
-			"&.active": {
-				color: "secondary",
-			},
-		},
-	},
-
-	menuFooter: {
-		width: "100%",
-		display: "flex",
-		flexDirection: "column",
-		alignItems: "center",
-		mt: "auto",
-	},
-
-	social: {
-		width: "100%",
-		display: "flex",
-		alignItems: "center",
-		justifyContent: "center",
-
-		icon: {
-			display: "flex",
-			alignItems: "center",
-			justifyContent: "center",
-			color: "text",
-			fontSize: 14,
-			mr: "15px",
-			transition: "all 0.25s",
-			cursor: "pointer",
-			":last-child": {
-				mr: "0",
-			},
-			"&:hover": {
-				color: "secondary",
-			},
-		},
-	},
-
-	button: {
-		color: "white",
-		fontSize: "14px",
-		fw: "700",
-		height: "45px",
-		borderRadius: "5px",
-		cursor: "pointer",
-		width: "100%",
-		display: "flex",
-		alignItems: "center",
-		justifyContent: "center",
-		py: "0",
-	},
+  wrapper: {
+    display: ["flex", null, null, null, "none"],
+    alignItems: "center",
+  },
+  handler: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    cursor: "pointer",
+  },
+  menuOverlay: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.9)",
+    zIndex: 1000,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: "20px",
+  },
+  closeButton: {
+    position: "absolute",
+    top: "20px",
+    right: "20px",
+    cursor: "pointer",
+    color: "white",
+  },
+  menuItems: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  },
+  menuLink: {
+    color: "white",
+    fontSize: "18px",
+    fontWeight: "500",
+    padding: "15px 0",
+    cursor: "pointer",
+    textDecoration: "none",
+    transition: "all 0.3s ease",
+    "&:hover": {
+      color: "primary",
+    },
+    "&.active": {
+      color: "primary",
+    },
+  },
 };
