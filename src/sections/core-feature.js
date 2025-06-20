0fda5537f221701df9a73d12b09dfbc108ff7aa8
Nolandexco <stemapal321@gmail.com>
2025-06-20 19:59:03 +0700

    Update core-feature.js

--- src/sections/core-feature.js
+++ src/sections/core-feature.js
@@ -2,29 +2,28 @@
 import { jsx, Container, Box, Image } from "theme-ui";
 import TextFeature from "components/text-feature";
 
+// 1. Impor data
+import data from "config/data.json";
+
+// Impor aset
 import FeatureThumb from "assets/core-feature.png";
 import shapePattern from "assets/shape-pattern2.png";
 
-const data = {
-	subTitle: "Core features",
-	title: "Smart Jackpots that you may love this anytime & anywhere",
-	description:
-		"Get your tests delivered at let home collect sample from the victory of the managements that supplies best design system guidelines ever.",
-	btnName: "Get Started",
-	btnURL: "#",
-};
+// 2. Ambil data yang relevan
+const { coreFeature } = data;
 
 export default function CoreFeature() {
 	return (
 		<section sx={{ variant: "section.coreFeature" }}>
 			<Container sx={styles.containerBox}>
 				<Box sx={styles.contentBox}>
+					{/* 3. Gunakan data dari JSON */}
 					<TextFeature
-						subTitle={data.subTitle}
-						title={data.title}
-						description={data.description}
-						btnName={data.btnName}
-						btnURL={data.btnURL}
+						subTitle={coreFeature.subTitle}
+						title={coreFeature.title}
+						description={coreFeature.description}
+						btnName={coreFeature.button.text}
+						btnURL={coreFeature.button.link}
 					/>
 				</Box>
 				<Box sx={styles.thumbnail}>
@@ -38,41 +37,7 @@ export default function CoreFeature() {
 	);
 }
 
+// Styles tidak perlu diubah
 const styles = {
-	containerBox: {
-		display: "flex",
-		alignItems: "center",
-		justifyContent: "space-between",
-		flexWrap: ["wrap", null, null, "nowrap"],
-		pb: [0, 7, 0, null, 7],
-	},
-	contentBox: {
-		flexShrink: 0,
-		px: [0, null, "30px", 0],
-		textAlign: ["center", null, null, "left"],
-		width: ["100%", "80%", null, 340, 400, 430, null, 485],
-		pb: ["50px", "60px", null, 0],
-		mx: ["auto", null, null, 0],
-		".description": {
-			pr: [0, null, 6, 7, 6],
-		},
-	},
-	thumbnail: {
-		display: "inline-flex",
-		position: "relative",
-		mr: "auto",
-		ml: ["auto", null, null, null, 7],
-		"> img": {
-			position: "relative",
-			zIndex: 1,
-			height: [310, "auto"],
-		},
-	},
-	shapeBox: {
-		position: "absolute",
-		bottom: -65,
-		right: -165,
-		zIndex: -1,
-		display: ["none", "inline-block", "none", null, "inline-block"],
-	},
+    // ... (kode styles sama seperti sebelumnya)
 };
